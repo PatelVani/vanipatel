8264dfd94e80480c46ee1567f2c54d86bff29c5f
PatelVani <110091657+PatelVani@users.noreply.github.com>
2022-08-10 08:23:52 +0530

    Add files via upload

---
+++ SUM.c
@@ -0,0 +1,11 @@
+ #include<stdio.h>
+ int main()
+ {
+     int a,b,sum;
+     printf("enter two numbers\n");     
+     scanf("%d\n",&a);
+     scanf("%d",&b);
+     sum=a+b;
+     printf("%d",sum);
+	     return 0;
+ }
